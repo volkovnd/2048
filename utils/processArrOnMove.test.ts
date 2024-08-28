b461a55a915ae4e7cf168bde831d7926760eff6f
VolkovND <volkovnd@yandex.ru>
2024-08-29 02:01:33 +0300

    fix: Исправлен тест

--- utils/processArrOnMove.test.ts
+++ utils/processArrOnMove.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { processArrOnMoveLeft } from "./processArrOnMoveLeft";
+import { processArrOnMoveLeft } from "./processArrOnMove";
 
 describe("processArrOnMoveLeft", () => {
   it("test arr", () => {
