2d51f5ae3b0ab852bcc7fb10fc9b555e47df0833
VolkovND <volkovnd@yandex.ru>
2025-02-02 17:51:18 +0300

    fex

--- components/UiButton.vue
+++ components/UiButton.vue
@@ -7,8 +7,8 @@
       v-if="icon"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      :width="24"
-      :height="24"
+      :width="20"
+      :height="20"
     >
       <path
         :d="icon"
@@ -16,7 +16,12 @@
       />
     </svg>
 
-    <slot />
+    <div
+      v-if="!!$slots.default || label"
+      class="label"
+    >
+      <slot>{{ label }}</slot>
+    </div>
   </button>
 </template>
 
@@ -24,14 +29,17 @@
 defineProps<{
   type?: "button";
   icon?: string;
+  label?: string;
 }>();
 </script>
 
 <style scoped>
 .ui-button {
-  align-items: stretch;
+  display: flex;
+  gap: calc(var(--spacing) / 2);
+  align-items: center;
   min-height: 32px;
-  padding: calc(var(--spacing) / 4) calc(var(--spacing) / 2);
+  padding: calc(var(--spacing) / 4) calc(var(--spacing) * 3 / 4);
   font-size: 16px;
   font-weight: 400;
   line-height: 1.5;
@@ -40,5 +48,9 @@ defineProps<{
   background-color: var(--primary);
   border: none;
   border-radius: 10%;
+}
+
+.ui-button svg {
+  display: block;
 }
 </style>
